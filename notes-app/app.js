d2a479d6762ce8b284806a3b2b414e3e6cf2d769
vice76 <arpityadavaj1999@gmail.com>
2021-09-23 23:41:21 +0530

    added add feature

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,9 +1,17 @@
-const validator = require("validator");
+const yargs = require("yargs");
 const chalk = require("chalk");
-const fs= require('./utils.js');
+const fs = require("./utils.js");
 
-const getNotes = fs()
+//const command = process.argv[2];
+yargs.version("1.1.0");
 
-console.log(getNotes);
-console.log(validator.isEmail("arpit@gmail.com"));
-console.log(chalk.green("Success!"));
+//crate add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding a new note!");
+  },
+});
+
+console.log(yargs.argv);
