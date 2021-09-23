89ce448e10cd8b6ff61d294be123425fa4eb50cf
vice76 <arpityadavaj1999@gmail.com>
2021-09-23 23:46:22 +0530

    added remove feature

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,12 +5,21 @@ const fs = require("./utils.js");
 //const command = process.argv[2];
 yargs.version("1.1.0");
 
-//crate add command
+//create add command
 yargs.command({
   command: "add",
   describe: "Add a new note",
   handler: function () {
     console.log("Adding a new note!");
+  },
+});
+
+//create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a new note",
+  handler: function () {
+    console.log("Removing  a old note!");
   },
 });
 
